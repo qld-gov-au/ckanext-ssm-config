933539c228289711e34daced62a2498897e0ce8a
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-09-08 13:43:22 +1000

    [QOL-8251] sync CI config with other repos
    - rationalise paths within virtual machines and refactor hardcoded values
    - upload screenshots on failure
    - update Redis

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -11,21 +11,39 @@ def go_to_home(context):
 
 @step('I log in')
 def log_in(context):
-
     assert context.persona
     context.execute_steps(u"""
         When I go to homepage
         And I click the link with text that contains "Log in"
-        And I fill in "login" with "$name"
+        And I log in directly
+    """)
+
+
+@step('I log in directly')
+def log_in_directly(context):
+    """
+    This differs to the `log_in` function above by logging in directly to a page where the user login form is presented
+    :param context:
+    :return:
+    """
+
+    assert context.persona
+    context.execute_steps(u"""
+        When I fill in "login" with "$name"
         And I fill in "password" with "$password"
         And I press the element with xpath "//button[contains(string(), 'Login')]"
-        Then I should see an element with xpath "//a[contains(string(), 'Log out')]"
+        Then I should see an element with xpath "//a[@title='Log out']"
     """)
 
 
 @step('I go to dataset page')
 def go_to_dataset_page(context):
     when_i_visit_url(context, '/dataset')
+
+
+@step('I go to dataset "{name}"')
+def go_to_dataset(context, name):
+    when_i_visit_url(context, '/dataset/' + name)
 
 
 @step('I go to organisation page')
